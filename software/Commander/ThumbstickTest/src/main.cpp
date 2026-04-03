69446b79ba025a984f2b78de2ea497e35c2debec
Raphael Weber <5662800+raphweb@users.noreply.github.com>
2026-04-03 23:38:12 +0200

    Small ThumbstickTest updates.

--- software/Commander/ThumbstickTest/src/main.cpp
+++ software/Commander/ThumbstickTest/src/main.cpp
@@ -6,31 +6,22 @@
 
 ESP32AnalogRead s1v(16), s1h(17), s2v(8), s2h(3);
 
-const char* calculateDirection(uint16_t h, uint16_t v) {
+enum E_Direction { C, N, NE, E, SE, S, SW, W, NW };
+const char* DirNames[9] { "·", "🡹", "🡽", "🡺", "🡾", "🡻", "🡿", "🡸", "🡼"};
+
+const E_Direction calculateDirection(const uint16_t h, const uint16_t v) {
   if (h < LOWER_THRESHHOLD) {
-    if (v < LOWER_THRESHHOLD) {
-      return "🡿";
-    } else if (v > UPPER_THRESHHOLD) {
-      return "🡼";
-    } else {
-      return "🡸";
-    }
+    if (v < LOWER_THRESHHOLD) return SW;
+    else if (v > UPPER_THRESHHOLD) return NW;
+    else return W;
   } else if (h > UPPER_THRESHHOLD) {
-    if (v < LOWER_THRESHHOLD) {
-      return "🡾";
-    } else if (v > UPPER_THRESHHOLD) {
-      return "🡽";
-    } else {
-      return "🡺";
-    }
+    if (v < LOWER_THRESHHOLD) return SE;
+    else if (v > UPPER_THRESHHOLD) return NE;
+    else return E;
   } else {
-    if (v < LOWER_THRESHHOLD) {
-      return "🡻";
-    } else if (v > UPPER_THRESHHOLD) {
-      return "🡹";
-    } else {
-      return "·";
-    }
+    if (v < LOWER_THRESHHOLD) return S;
+    else if (v > UPPER_THRESHHOLD) return N;
+    else return C;
   }
 }
 
@@ -45,6 +36,6 @@ void loop() {
   uint16_t x1 = (4095-s1h.readRaw()), x2 = s2h.readRaw();
   boolean btn1 = digitalRead(18), btn2 = digitalRead(9);
   Serial.printf("S1(%s|%c)   S2(%s|%c)\r",
-    calculateDirection(x1, y1), (btn1 ? ' ' : 'X'), calculateDirection(x2, y2), (btn2 ? ' ' : 'X'));
+    DirNames[calculateDirection(x1, y1)], (btn1 ? ' ' : 'X'), DirNames[calculateDirection(x2, y2)], (btn2 ? ' ' : 'X'));
   delay(100);
 }
